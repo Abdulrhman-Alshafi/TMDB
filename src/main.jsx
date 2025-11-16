ebb0a626d72af840232f988c8b960fef304f2a67
Abdulrhman-Alshafi <Abdulrahman.Alshafi@optimumpartners.com>
2025-11-16 11:28:36 +0300

    reomve some things

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./style.css"
+
+import "./style.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
 
